7630195591492cfbe99b4e835bad85ab40a639ee
Dakota Schneider <dakota@hackthetruth.org>
2014-02-19 20:04:37 +0000

    package common JSON parsing into _parse_json()

--- scrape_comments.py
+++ scrape_comments.py
@@ -4,9 +4,9 @@ import re
 import time
 import logging
 
-
 from datetime import datetime, timedelta
 from multiprocessing import Process, current_process
+
 
 from spiderbase import SpiderBase
 
@@ -16,35 +16,33 @@ from models import Comment
 
 class Monitor(SpiderBase):
 
-    def __init__(self, username=None, password=None, delay=4, log='comment-scraper.log'):
+    def __init__(self, username=None, password=None, delay=4, logfile='comment-scraper.log'):
         #logging
-        logfile = log
         self.logger = logging.getLogger('comment-scraper')
         self.logger.addHandler(logging.FileHandler(logfile))
-        self.logger.addHandler(logging.StreamHandler())
+        self.logger.addHandler(logging.StreamHandler()) # print to std out for debugging
         self.logger.setLevel(logging.DEBUG)
 
         #init
         self.delay = delay
+        self.limit = 100 # TODO: Add automatic limit adjustment to minimize network load
         self.spider = SpiderBase(username=username, password=password)
     
     def monitor_comments(self):
-        url = 'http://www.reddit.com/comments.json?limit=100'
+        url = 'http://www.reddit.com/comments.json?limit=25'
         newest_id = None
 
         # get the ID the first time, but don't process
-        data = self.spider._get_json(url)
-        next_newest_id = data['data']['children'][0]['data']['id']
+        data, next_newest_id, after = self._parse_json(url)
 
         while True:
             start = datetime.now()
                         
-            data = self.spider._get_json(url)
-            newest_id = data['data']['children'][0]['data']['id']
+            data, newest_id, after = self._parse_json(url)
             
             # we don't want to process the first page, else we risk infinite recursion
             self.logger.debug("Starting new Comment Parser")
-            p = Process(name='Comment Parser', target=self._process_comments, args=(url, next_newest_id, data))
+            p = Process(name='Comment Parser', target=self._process_comments, args=(url, next_newest_id, data, after))
             p.start()
             
             # increment
@@ -58,13 +56,23 @@ class Monitor(SpiderBase):
             
             time.sleep( sleep )
 
+    def _parse_json(self, url, after=None):
+        if after is not None:
+            url = "%s&after=%s" % (url, after)
+        
+        data = self.spider._get_json(url)
+        newest_id = data['data']['children'][0]['data']['id']
+        after = data['data']['after']
+        
+        return (data, newest_id, after)
 
-    def _process_comments(self, url, next_newest_id, data):
+
+    def _process_comments(self, url, next_newest_id, data, after):
         """Threaded comment parser"""
         
         comments = data['data']['children']
         new_comments = 0
-        recurse_count = 0
+        pages_parsed = 0
         
         for i, c in enumerate(comments):
             comment = c['data']
@@ -76,6 +84,8 @@ class Monitor(SpiderBase):
                 break
             
             # Create new Comment, stick in it in the database.
+            # I feel like we should be able to loop through the JSON object and avoid writing out all these assignments
+            # Would that even be a good idea?
             comment_entry = Comment()
             comment_entry.subreddit_id           = comment['subreddit_id']
             comment_entry.link_title             = comment['link_title']
@@ -99,26 +109,30 @@ class Monitor(SpiderBase):
             comment_entry.ups                    = comment['ups']
             comment_entry.distinguished          = comment['distinguished']
             
+            # Append to current session. We'll commit at the end.
             session.add(comment_entry)
 
             # update our counter
-            # this is just temporary           
+            # For informational purposes. Maybe I'll have it propagate a log in the database,
+            #  that would make it really easy to graph comment rate over time           
             new_comments = new_comments + 1
         else:
             # we didn't break out of the loop, therefore we never encountered next_newest_id
-            # so, get the next page and try again
-            after = data['data']['after']
-            data = self.spider._get_json("%s&after=%s" % (url, after))
+            # so, get the next page and keep going
+            data, newest_id, after = self._parse_json(url, after)
             
-            if recurse_count <= 3:
+            if pages_parsed <= 3:
                 # we should never have to go back further than two additional pages
                 # if we have, something is probably wrong            
-                self.logger.debug("RECURSE")
-                recurse_count = recurse_count + 1
-                self._process_comments(url, next_newest_id, data)
+                self.logger.debug("Getting a second page.")
+                pages_parsed = pages_parsed + 1
+                self._process_comments(url, next_newest_id, data, after)
+            else:
+                self.logger.debug("Attempted to parse more than three pages.")
 
+        # Push new objects to database
         session.commit()
-            
+        
         self.logger.debug( "%s: %s %s" % (current_process().name, datetime.now(), new_comments) )
         
         # no need to return anything right now
@@ -135,8 +149,7 @@ if __name__ == "__main__":
     
     username = cfg_file.get('reddit', 'username')
     password = cfg_file.get('reddit', 'password')
-        
-
+    
     print "Logging in..."
     comments = Monitor(username, password)
     print "  Success!"
